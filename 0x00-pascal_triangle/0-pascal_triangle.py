b939cd4fbfe5a1837d595c9052ad0ec1a40a32f8
Lurldgbodex <professionaleditor6@gmail.com>
2022-12-21 08:44:30 -0800

    update code to pycodestyle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -5,7 +5,7 @@
 def pascal_triangle(n):
     '''return a lists of integers representing pascal's
     triangle of n'''
-    pascal_list = []
+    pasc_list = []
     if n <= 0:
         return pascal_list
     for i in range(n):
@@ -14,6 +14,6 @@ def pascal_triangle(n):
             if j == 0 or j == i:
                 triangle.append(1)
             elif i > 0 and j > 0:
-                triangle.append(pascal_list[i - 1][j - 1] + pascal_list[i - 1][j])
-        pascal_list.append(triangle)
-    return pascal_list
+                triangle.append(pasc_list[i - 1][j - 1] + pasc_list[i - 1][j])
+        pasc_list.append(triangle)
+    return pasc_list
